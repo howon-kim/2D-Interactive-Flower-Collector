8d4c009426ffb9b4202e4d7752d07c54de176503
Kat <kathdjc@berkeley.edu>
2019-04-23 09:56:47 -0700

    hallways

--- proj3/byow/Hallways.java
+++ proj3/byow/Hallways.java
@@ -1,12 +1,40 @@
 package byow;
+import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
 
-public class Hallways {
+public class Hallway {
 
+    public static TETile world;
 
-    // nearest method
+    public static void putVerticalHallway(TETile[][] world, Room room1, Room room2, int h) {
 
+        // generating midpoints of room objects
 
-    public void connect(Room r1, Room r2) {
+        double midpoint1 = (room1.x1 - room1.x2) / 2;
+        double midpoint2 = (room2.x1 - room2.x2) / 2;
 
+        for (int y = 0; y < h; y += 1) {
+            world[loc.x() - 1][loc.y() + y] = Tileset.WALL;
+            world[loc.x() + 1][loc.y() + y] = Tileset.WALL;
+        }
+
+        for (int y = 0; y < h; y += 1) {
+            world[loc.x()][loc.y() + y] = Tileset.FLOOR;
+        }
     }
+
+    public static void putHorizontalHallway(TETile[][] world, Room room1, Room room2, int w) {
+
+        double midpoint1 = (room1.y1 - room1.y2) / 2;
+        double midpoint2 = (room2.y1 - room2.y2) / 2;
+
+        for (int x = 0; x < w; x += 1) {
+            world[loc.x() + x][loc.y() - 1] = Tileset.WALL;
+            world[loc.x() + x][loc.y() + 1] = Tileset.WALL;
+        }
+        for (int x = 0; x < w; x += 1) {
+            world[loc.x() + x][loc.y()    ] = Tileset.FLOOR;
+        }
+    }
+
 }
